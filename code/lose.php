0e81cd4ce7a3e065dd97ef764f6b354e3d34bcf5
Pau Maestre <96410501+pmaestre03@users.noreply.github.com>
2023-10-20 18:22:48 +0200

    Add files via upload

---
+++ code/lose.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lose</title>
+</head>
+<body>
+    <h2>Que pena</h2>
+    <img src="images/lose.gif" alt="" srcset="">
+</body>
+</html>
---
+++ code/win.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Win</title>
+</head>
+<body>
+    
+    <div class="box">
+    <h2>Felicidades</h2>
+    <img src="images/congratulations.gif" alt="" >
+    </div>
+    
+</body>
+</html>
