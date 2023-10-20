1179d36255d2e15afb06b8757e86add9d065d6fb
¨pmaestre03¨ <maestrep15@gmail.com>
2023-10-20 12:51:27 +0200

    algo

--- code/game.php
+++ code/game.php
@@ -12,20 +12,38 @@
     function preguntas()
     {
         global $nivel;
+        $question_mark = '*';
+        $correct_mark = '=';
+        $incorrect_mark = '-';
         $preguntas_nivel = file("questions/catalan_".strval($nivel).".txt");
-        $preguntas_nivel_separadas = [];
+        $preguntas_por_nivel = [];
+        
         for ($i = 0; $i < count($preguntas_nivel); $i++) {
-            if ($preguntas_nivel[$i][0] == "*") {
-                array_push($preguntas_nivel_separadas, $preguntas_nivel[$i]);
+            if ($preguntas_nivel[$i][0] === $question_mark) {
+                $llave = $preguntas_nivel[$i];
             }
             else {
-                $preguntas_nivel_separadas[$i] = $preguntas_nivel[$i];
+                $preguntas_por_nivel[$llave][] .= $preguntas_nivel[$i];
             }
-            echo "" . $preguntas_nivel[$i] . "<br>";
         }
-        var_dump($preguntas_nivel_separadas);
+        return $preguntas_por_nivel;
     }
-    preguntas();
+    
+    function preguntas_aleatorias() {
+        $preguntas_por_nivel = preguntas();
+        //shuffle($preguntas_por_nivel);
+        var_dump($preguntas_por_nivel);
+        foreach ($preguntas_por_nivel as $key => $array) {
+            echo "<h2>$key<h2>";
+            foreach ($array as $respuestas) {
+                echo "<button>$respuestas</button>";
+            }
+        }
+        
+        
+        
+    }
+    preguntas_aleatorias();
     ?>
 </body>
 </html>
