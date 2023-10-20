cc6155a5776cbe2e37886fd0c2540baf07ed8a56
Pau Maestre <96410501+pmaestre03@users.noreply.github.com>
2023-10-20 17:03:12 +0200

    Spec5

--- code/game.php
+++ code/game.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Joc</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
@@ -13,37 +14,89 @@
     {
         global $nivel;
         $question_mark = '*';
-        $correct_mark = '=';
-        $incorrect_mark = '-';
-        $preguntas_nivel = file("questions/catalan_".strval($nivel).".txt");
+        $preguntas_nivel = file("questions/catalan_" . strval($nivel) . ".txt");
         $preguntas_por_nivel = [];
-        
+
         for ($i = 0; $i < count($preguntas_nivel); $i++) {
             if ($preguntas_nivel[$i][0] === $question_mark) {
                 $llave = $preguntas_nivel[$i];
+            } 
+            elseif ($preguntas_nivel[$i][0] === "+") {
+                $preguntas_por_nivel[$llave][] = $preguntas_nivel[$i];
             }
-            else {
-                $preguntas_por_nivel[$llave][] .= $preguntas_nivel[$i];
+            elseif ($preguntas_nivel[$i][0] === "-") {
+                $preguntas_por_nivel[$llave][] = $preguntas_nivel[$i];
             }
         }
+        var_dump($preguntas_por_nivel);
         return $preguntas_por_nivel;
     }
-    
-    function preguntas_aleatorias() {
-        $preguntas_por_nivel = preguntas();
+
+    function numeros_aleatorios($max)
+    {
         //shuffle($preguntas_por_nivel);
-        var_dump($preguntas_por_nivel);
-        foreach ($preguntas_por_nivel as $key => $array) {
-            echo "<h2>$key<h2>";
-            foreach ($array as $respuestas) {
-                echo "<button>$respuestas</button>";
+        $array_of_number = [];
+        for ($i = 0; $i < 3; $i++) {
+            while (true) {
+                $num = rand(0, $max - 1);
+                if (!in_array($num, $array_of_number)) {
+                    array_push($array_of_number, $num);
+                    break;
+                }
             }
         }
-        
-        
-        
+        return $array_of_number;
     }
-    preguntas_aleatorias();
+
+    function preguntas_aleatorias()
+    {
+        $i = 0;
+        $preguntas = preguntas();
+        $positions = numeros_aleatorios(count($preguntas));
+        $preguntas_nivel = [];
+        foreach ($preguntas as $key => $value) {
+            if (in_array($i, $positions)) {
+                foreach ($value as $respuestas) {
+                    $preguntas_nivel[$key][] = $respuestas;
+                }
+            }
+            $i++;
+        }
+        return $preguntas_nivel;
+    }
+
+    function print_preguntas_aleatorias()
+    {
+        $preguntas_escogidas = preguntas_aleatorias();
+        $total = count($preguntas_escogidas);
+        $preguntas_restantes = $total;
+        foreach ($preguntas_escogidas as $key => $value) {
+            if ($preguntas_restantes == $total) {
+                echo "<div>";
+                echo "<h2>$key</h2>";
+                foreach ($value as $respuestas) {
+                    echo "<button>$respuestas</button>";
+                }
+                echo "</div>";
+            } else {
+                echo "<div class='oculto'>";
+                echo "<h2>$key</h2>";
+                foreach ($value as $respuestas) {
+                    echo "<button>$respuestas</button>";
+                }
+                echo "<p>Hola</p>";
+                echo "</div>";
+            }
+            $preguntas_restantes--;
+        }
+    }
+    print_preguntas_aleatorias();
     ?>
+    <div class='oculto'>
+    <button>Següents preguntes</button>
+    <button>Tornar a l'inici</button>
+    </div>
+    
+    
 </body>
 </html>
