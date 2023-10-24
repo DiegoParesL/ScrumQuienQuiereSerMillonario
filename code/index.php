2af89f76748d6020e826ec1996b22ec802a3353f
«arnaumv» <arnaumestre02@gmail.com>
2023-10-24 19:21:57 +0200

    ..

--- code/index.php
+++ code/index.php
@@ -23,6 +23,7 @@ unset($_SESSION['nivel']);
     <h1 id="titulo">Benvigut a Qui vol ser milionari?</h1>
     <img src="images/milionari.png" alt="milionariIMG" width="300px" height="300px">
     <p id="descripcion">Aquest joc aquesta basat en el programa del mateix nom.<br>En el qual hi ha diferents nivells de dificultat respecte les preguntes.<br>Començarem en el nivell 1 i conforme anem encertat preguntes ira pujant el nivell, fins al nivell 6.<br>Cada nivell de dificultat té en total 3 preguntes i una vegada encertades les 3 passarem a la següent dificultat, fins que encertem les 18 preguntes totals o fallem.</p>
+    <br>
     <form action="game.php" method="post">
         <button type="submit" id="boton-jugar" class="boton-grande">Jugar</button>
     </form>
