a340b70d430e1502dd37711ff78d7d43f05b8c67
arnaumv <95907269+arnaumv@users.noreply.github.com>
2023-10-24 22:22:04 +0200

    PRUEBAS

---
+++ code/gamePrueba.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joc</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Juego de Preguntas</h1>
+
+    <div id="preguntasContainer">
+        
+    <?php
+session_start();
+
+// Obtener el idioma de la variable de sesión
+$idioma = isset($_SESSION['idioma']) ? $_SESSION['idioma'] : 'english';
+
+$nivel = isset($_SESSION['nivel']) ? $_SESSION['nivel'] : 1;
+
+function preguntas()
+{
+    global $nivel, $idioma;
+    $question_mark = '*';
+    $preguntas_nivel = file("questions/{$idioma}_{$nivel}.txt");
+    $preguntas_por_nivel = [];
+    $llave = '';
+    foreach ($preguntas_nivel as $linea) {
+        if ($linea[0] === $question_mark) {
+            $llave = $linea;
+        } elseif ($linea[0] === "+") {
+            $preguntas_por_nivel[$llave][] = $linea;
+        } elseif ($linea[0] === "-") {
+            $preguntas_por_nivel[$llave][] = $linea;
+        }
+    }
+    return $preguntas_por_nivel;
+}
+
+
+
+    // Resto de tu código para imprimir preguntas, respuestas y botones
+
+
+            function numeros_aleatorios($max)
+            {
+                $array_of_number = [];
+                for ($i = 0; $i < 3; $i++) {
+                    while (true) {
+                        $num = rand(0, $max - 1);
+                        if (!in_array($num, $array_of_number)) {
+                            array_push($array_of_number, $num);
+                            break;
+                        }
+                    }
+                }
+                return $array_of_number;
+            }
+
+            function preguntas_aleatorias()
+            {
+                $i = 0;
+                $preguntas = preguntas();
+                $positions = numeros_aleatorios(count($preguntas));
+                $preguntas_nivel = [];
+                foreach ($preguntas as $key => $value) {
+                    if (in_array($i, $positions)) {
+                        foreach ($value as $respuestas) {
+                            $preguntas_nivel[$key][] = $respuestas;
+                        }
+                    }
+                    $i++;
+                }
+                return $preguntas_nivel;
+            }
+
+            function print_preguntas_aleatorias()
+            {
+                $preguntas_escogidas = preguntas_aleatorias();
+                $total = count($preguntas_escogidas);
+                $preguntas_restantes = $total;
+                foreach ($preguntas_escogidas as $key => $value) {
+                    if ($preguntas_restantes == $total) {
+                        echo "<div>";
+                        echo "<h2>" . substr($key, 1) . "</h2>"; // Quita el signo "*" en el título
+                        echo "<div class='grid'>";
+                        foreach ($value as $respuestas) {
+                            if ($respuestas[0] === "+") {
+                                echo "<p class=\"oculto\" id='respuesta" . ($total - $preguntas_restantes) . "'>$respuestas</p>";
+                                echo "<button id=\"res" . ($total - $preguntas_restantes) . "\" onclick=\"trueClick(this)\">" . trim($respuestas, "+-") . "</button>";
+                            } else {
+                                echo "<button class=\"fail" . ($total - $preguntas_restantes) . "\" onclick=\"failClick(this)\">" . trim($respuestas, "+-") . "</button>";
+                            }
+                        }
+                        echo "</div>";
+                        echo "</div>";
+                    } else {
+                        echo "<div id='pregunta" . ($total - $preguntas_restantes + 1) . "' class='oculto'>";
+                        echo "<h2>" . substr($key, 1) . "</h2>"; // Quita el signo "*" en el título
+                        echo "<div class='grid'>";
+                        foreach ($value as $respuestas) {
+                            if ($respuestas[0] === "+") {
+                                echo "<p class=\"oculto\" id='respuesta" . ($total - $preguntas_restantes) . "'>$respuestas</p>";
+                                echo "<button id=\"res" . ($total - $preguntas_restantes) . "\" onclick=\"trueClick(this)\">" . trim($respuestas, "+-") . "</button>";
+                            } else {
+                                echo "<button class=\"fail" . ($total - $preguntas_restantes) . "\" onclick=\"failClick(this)\">" . trim($respuestas, "+-") . "</button>";
+                            }
+                        }
+                        echo "</div>";  
+                        echo "</div>";
+                    }
+                    $preguntas_restantes--;
+                }
+            }
+            print_preguntas_aleatorias();
+            ?>
+        </div>
+
+        <div class='oculto' id="botones">
+        <form method="post">
+            <button name="siguiente" class="boton-accion" id="siguiente">Següents preguntes</button>
+        
+        <button id="inicio" class="boton-accion" onclick="window.location.href='game.php'">Tornar a l'inici</button>
+        </form>
+    </div>
+
+
+
+
+        <script src="funciones/funcionalidades.js"></script>
+        <script src="funciones/sounds.js"></script>
+</body>
+</html>
---
+++ code/indexPrueba.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+// Restablecer el nivel del juego al nivel 1
+unset($_SESSION['nivel']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Qui vol ser milionari?</title>
+</head>
+
+<body>
+    <header>
+        <p>
+            <a href="#" onclick="changeLanguage('es');"><img src="images/spain.png" alt="Español" height="50px"></a>&nbsp;
+            <a href="#" onclick="changeLanguage('ca');"><img src="images/catalan.png" alt="Catalán" height="50px"></a>&nbsp;
+            <a href="#" onclick="changeLanguage('en');"><img src="images/english.jpg" alt="Inglés" height="50px"></a>
+        </p>
+    </header>
+    <h1 id="titulo">Benvigut a Qui vol ser milionari?</h1>
+    <img src="images/milionari.png" alt="milionariIMG" width="300px" height="300px">
+    <p id="descripcion">Aquest joc aquesta basat en el programa del mateix nom.<br>En el qual hi ha diferents nivells de dificultat respecte les preguntes.<br>Començarem en el nivell 1 i conforme anem encertat preguntes ira pujant el nivell, fins al nivell 6.<br>Cada nivell de dificultat té en total 3 preguntes y una vez encertades las 3 passarem a la següent dificultat, fins que encertem les 18 preguntes totals o fallem.</p>
+    <br>
+    <form action="game.php" method="get">
+    <input type="hidden" name="lang" id="selectedLanguage" value="ca"> <!-- Cambia "es" a "ca" o "en" según el idioma seleccionado -->
+    <button type="submit" id="boton-jugar" class="boton-grande">Jugar</button>
+</form>
+    <br>
+    <form action="ranking.php" method="post">
+        <button type="submit" id="ranking" class="boton-mediano">Hall of fame</button>
+    </form>
+
+    <script src="funciones/funcionalidades.js"></script>
+    <script src="funciones/translation.js"></script>
+
+    <script>
+        // Función para cambiar el idioma de la página
+        function changeLanguage(lang) {
+            // Aquí puedes definir objetos de idioma con los textos correspondientes
+            const languageData = {
+                'es': {
+                    'titulo': 'Bienvenido a ¿Quién quiere ser millonario?',
+                    'descripcion': 'Este juego está basado en el programa del mismo nombre...',
+                    // Agrega más textos aquí
+                },
+                'ca': {
+                    'titulo': 'Benvingut a Qui vol ser milionari?',
+                    'descripcion': 'Aquest joc aquesta basat en el programa del mateix nom...',
+                    // Agrega más textos aquí
+                },
+                'en': {
+                    'titulo': 'Welcome to Who Wants to Be a Millionaire?',
+                    'descripcion': 'This game is based on the program of the same name...',
+                    // Agrega más textos aquí
+                }
+            };
+
+            // Cambiar el contenido de la página según el idioma seleccionado
+            document.getElementById('titulo').textContent = languageData[lang]['titulo'];
+            document.getElementById('descripcion').textContent = languageData[lang]['descripcion'];
+
+            // Actualiza el campo oculto con el idioma seleccionado
+            document.getElementById('selectedLanguage').value = lang;
+        }
+    </script>
+</body>
+</html>
