40321dcab109bce5ea6149190bc81b1302db6a38
Pau Maestre <96410501+pmaestre03@users.noreply.github.com>
2023-10-24 17:03:21 +0200

    Delete lose_sound.js

--- lose_sound.js
+++
@@ -1,8 +0,0 @@
-var audioLose = new Audio("audio/lose.mp3");
-lose.addEventListener("click", ()=>{
-    audioLose.currentTime = 0;
-    audioLose.play();
-})
-pausar.addEventListener("click", ()=>{
-    audioLose.pause();
-}) 
