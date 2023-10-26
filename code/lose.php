754fc874a486db6a52b8aaee5cd348f1b42f5fb4
Pau Maestre <96410501+pmaestre03@users.noreply.github.com>
2023-10-26 11:27:31 +0200

    Sprint 1

--- code/lose.php
+++ code/lose.php
@@ -34,7 +34,7 @@
         session_start();
         if (isset($_POST["nombre"])) {
             $file = fopen("records.txt", "a+");
-            fwrite($file,$_POST["nombre"].", 18, ".session_create_id()."\n");
+            fwrite($file,$_POST["nombre"].", 0, ".session_create_id()."\n");
             fclose($file);            
         }
         session_destroy();
