ebaf55126c62d9f0ccc7862509c5296b7c7d9fb9
arnaumv <95907269+arnaumv@users.noreply.github.com>
2023-11-01 11:15:08 +0100

    Add files via upload

---
+++ code/pruebas/comodinPublico.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8" />
+    <title>Comodin publico</title>
+    <link href="style.css" rel="stylesheet" />
+  </head>
+  <body>
+	<div class="titulo">
+		<p>texto para prueba</p>
+        <div>
+            <h2>PREGUNTA</h2>
+            <div id="respuestas">
+                <button id="res">Correcto</button>
+                <button>Incorrecto1</button>
+                <button>Incorrecto2</button>
+                <button>Incorrecto3</button>
+            </div>
+        </div>
+        
+        <button id="trigger" onclick="empezarAnimacion()">Comodin</button>
+        <?php
+            echo"<div id=\"oculto\" class=\"publico\">";
+            echo"<div class=\"publicOcult red\">";
+            echo"</div>";
+            echo"<div class=\"publicOcult blue\">";
+            echo"</div>";
+            echo"<div class=\"publicOcult orange\">";
+            echo"</div>";
+            echo"<div class=\"publicOcult yellow\">";
+            echo"</div>";
+            echo"</div>";
+        ?>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <p>hola</p>
+	</div>
+    <script src="comodinPublico.js"></script>
+  </body>
+</html>
