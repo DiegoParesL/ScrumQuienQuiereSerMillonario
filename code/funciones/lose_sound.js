b92a317a317a4146e1c4d2dbd1f2cf2bdafb636c
arnaumv <95907269+arnaumv@users.noreply.github.com>
2023-11-02 08:53:21 +0100

    Delete code directory

--- code/funciones/lose_sound.js
+++
@@ -1,8 +0,0 @@
-var audio = new Audio("audio/lose.mp3");
-lose.addEventListener("click", ()=>{
-    audio.currentTime = 0;
-    audio.play();
-})
-pausar.addEventListener("click", ()=>{
-    audio.pause();
-})
--- code/funciones/win_sound.js
+++
@@ -1,6 +0,0 @@
-var audio = new Audio("audio/felicidades.mp3");
-audio.volume = 1;
-win.addEventListener("click", ()=>{
-    audio.currentTime = 0;
-    audio.play();
-})
