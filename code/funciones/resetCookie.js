bc69b82d9dee6ed1e1535d286239c861f7318680
arnaumv <95907269+arnaumv@users.noreply.github.com>
2023-11-02 08:57:01 +0100

    Add files via upload

---
+++ code/funciones/resetCookie.js
@@ -0,0 +1,4 @@
+function resetCookie() {
+    document.cookie='aciertos='+0;
+}
+resetCookie();
---
+++ code/ranking.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ranking</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Hall of Fame</h1>
+<div class="table-container">
+    <table>
+        <thead>
+            <tr>
+                <th style='font-size: 40px; padding-right: 20px;'>Name</th>
+                <br>
+                <th style='font-size: 40px; padding-left: 20px;'>Score</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $file = fopen("records.txt", "r");
+            $ranking = [];
+            while (!feof($file)) {
+                $line = fgets($file);
+                trim($line, " ");
+                if (!ctype_space($line)) {
+                    $users = explode(",", $line);
+                    if(!empty($users[0])) {
+                        $ranking[$users[0]] = $users[1];
+                    }
+                }
+            }
+            arsort($ranking);
+            foreach ($ranking as $order => $valor) {
+                $name = substr($order, 0, strlen($order));
+                
+                echo "<tr>";
+                
+                echo "<td style='font-size: 30px;'>$name</td>";
+                echo "<td style='font-size: 30px;'>$valor</td>";
+                
+                echo "</tr>";
+            }
+            fclose($file);
+            ?>
+        </tbody>
+    </table>
+</div>
+<br><br><br>
+<div class="button-container">
+    <form action="index.php" method="post">
+        <button class="boton-mediano" type="submit" id="index">Home</button>
+    </form>
+</div>
+</body>
+</html>
