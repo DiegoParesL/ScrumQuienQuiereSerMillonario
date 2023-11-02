df2a6b42496765ca79d0f8e4314b9d31ee4c54ee
arnaumv <95907269+arnaumv@users.noreply.github.com>
2023-11-02 12:32:36 +0100

    Add files via upload

---
+++ code/win.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+
+if (!isset($_SERVER['HTTP_REFERER']) || !strpos($_SERVER['HTTP_REFERER'], "game.php")) {
+    // Si la página no se accede desde "game.php", redirige o muestra un mensaje de error.
+    header("HTTP/1.1 403 Forbidden");
+   
+    exit;
+}
+// El contenido de la página "win.php" continua aquí
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Win</title>
+</head>
+<body>
+    <h3>You got it</h3>
+    <img src="images/winner.gif" alt="" height="290px" width="290px">
+   
+
+    <div  id="pantalla">
+    <br> <br>
+    <?php 
+    echo "<p>Questions answered correctly: " . $_COOKIE["aciertos"] . "</p>";
+?>
+
+    <br><br>
+    <p>
+    <button class="boton-mediano" onclick="window.location.href = 'index.php'">Back To Start</button>
+    <button class="boton-mediano" id="publish_button" name="publish_button" type="submit" onclick="publish()">Publish</button>
+    </p>
+    </div>
+    <br>
+    <div class="oculto" id="publicar">
+    <form method="post" id="publicar">
+        <input type="text" name="nombre" id="nombre">
+        <button type="submit" name="send" id="send">Enviar</button>
+    </form>
+    </div>
+
+    <script src="funciones/win_sound.js"></script>
+    <script src="funciones/funcionalidades.js"></script>
+    <?php
+        session_start();
+        if (isset($_POST["nombre"])) {
+            $file = fopen("records.txt", "a+");
+            fwrite($file,$_POST["nombre"].", 18, ".session_id()."\n");
+            fclose($file);           
+             
+        }
+        session_destroy();
+    ?>
+</body>
+</html>
