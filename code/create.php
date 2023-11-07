2759e4d09e75bc38d2380f6e67a6a4e8763c7efa
pmaestre03 <maestrep15@gmail.com>
2023-11-07 16:02:35 +0100

    create 100

--- code/create.php
+++ code/create.php
@@ -50,8 +50,7 @@ if (
     <p>Pregunta</p>
     <input type="text" name="question" id="question">
     <br>
-    <p>Respuestas</p>
-    <p>Para indicar la respuesta correcta escribe + delante de la respuesta y las incorrectas con -</p>
+    <p>Escribe las respuestas</p>
 
     <label for="">Respuesta 1</label><input type="text" name="respuesta1" id="respuesta1"> 
     <label for="">Respuesta 2</label><input type="text" name="respuesta2" id="respuesta2">
@@ -60,6 +59,7 @@ if (
     <label for="">Respuesta 4</label><input type="text" name="respuesta4" id="respuesta4">
     <br>
     <br>
+    <p>Indica la respuesta correcta</p>
     <div><input type="radio" name="correcta" id="respuesta_1" value="respuesta1" checked><label for="respuesta_1">Respuesta 1</label>
     <input type="radio" name="correcta" id="respuesta_2"  value="respuesta2" ><label for="respuesta_2">Respuesta 2</label></div>
     <div><input type="radio" name="correcta" id="respuesta_3" value="respuesta3" ><label for="respuesta_3">Respuesta 3</label>
@@ -73,26 +73,19 @@ if (
     <?php
     error_reporting(0);
     $file = fopen("questions/".$_POST["idioma"]."_".$_POST["nivel"].".txt","a+");
-    print_r($_POST["respuesta1"]."<br>");
     $correcta = $_POST["correcta"];
-    print_r($_POST[$correcta]);
+    $respuestas = "";
+    for ($i=1;$i<5;$i++) {
+        if ($_POST["respuesta".$i] != $_POST[$correcta]) {
+            $respuestas .= "\n- ".$_POST["respuesta".$i];
+        } else {
+            $respuestas .= "\n+ ".$_POST[$correcta];
+        }
+    }
     if ($_POST["question"]!="" && $_POST["respuesta1"]!="" && $_POST["respuesta2"]!=""
         && $_POST["respuesta3"]!="" && $_POST["respuesta4"]!="") {
-            $correcta = $_POST["correcta"];
-            for ($i=0;$i<4;$i++) {
-                
-            }
-            if ($respuesta_correcta != 1) {
-                ?>
-                <script>
-                    window.alert("Respuestas no validas")
-                </script>
-                <?php
-            } elseif ($respuesta_correcta==1) {
-                fwrite($file,"\n* ".$_POST["question"]."\n".$_POST["respuesta1"]."\n".
-                $_POST["respuesta2"]."\n".$_POST["respuesta3"]."\n".$_POST["respuesta4"]."\n"."# ".$_POST["img"]); 
-            }
-        }
+            fwrite($file,"\n* ".$_POST["question"].$respuestas."\n# ".$_POST["img"]);
+    }
     ?>
 </body>
 </html>
